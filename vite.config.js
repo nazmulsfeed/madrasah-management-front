d1aa2d795aa38851cb07473d3453994e9b200d0b
Nazmul <your-email@example.com>
2026-08-04 07:47:40 +0600

    refactor: move index.html to src folder and configure vite to prevent overwriting prod index.html during server pull

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src',
   base: '/',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   plugins: [react()],
   server: {
     port: 5173,
